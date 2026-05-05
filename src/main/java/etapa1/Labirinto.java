5ff34e6a1334438d42303b53e022cad6eecc22b3
cauelr <caue.lrodrigues@hotmail.com>
2026-05-04 21:07:26 -0300

    Finalizada e corrigida a ETAPA1

--- src/main/java/etapa1/Labirinto.java
+++ src/main/java/etapa1/Labirinto.java
@@ -36,33 +36,49 @@ public class Labirinto {
 
         } catch(FileNotFoundException e){
             System.out.println("O Arquivo não foi encontrado: " + e.getMessage()); // Trata dentro do método a Exceção FileNotFoundException
+            throw new IllegalArgumentException(e.getMessage());
         } catch(IOException e){
             System.out.println("Erro ao tentar ler o arquivo: " + e.getMessage()); // Trata dentro do método a Exceção IOException
+            throw new IllegalArgumentException(e.getMessage());
         }
     }
 
     public boolean percorreLabirinto(){
-        labirinto[0][0] = CAMINHO_SOLUCAO; // Partindo da posição [0][0]
-        return resolverLabirinto(0, 0);
+        return resolverLabirinto(0, 0); // Partindo da posição [0][0]
     }
 
     private boolean resolverLabirinto(int x, int y){
         if(labirinto[x][y] == SAIDA) return true; // 1. CASO BASE: chegou no destino?
         labirinto[x][y] = CAMINHO_SOLUCAO; // 2. Marca posição atual
 
-        // 3. Tenta as 4 direções
-        if(x - 1 >= 0 && labirinto[x-1][y] != PAREDE && labirinto[x][y] != CAMINHO_SOLUCAO){ // Valida se pode ir para cima
+        // 3. Variáveis que recebem uma estrutura condicional:
+        boolean podeCima = x - 1 >= 0 && labirinto[x-1][y] != PAREDE && labirinto[x-1][y] != CAMINHO_SOLUCAO; // PODE IR PARA CIMA?
+        boolean podeBaixo = x + 1 < labirinto.length && labirinto[x+1][y] != PAREDE && labirinto[x+1][y] != CAMINHO_SOLUCAO; // PODE IR PARA BAIXO?
+        boolean podeEsquerda = y - 1 >= 0 && labirinto[x][y-1] != PAREDE && labirinto[x][y-1] != CAMINHO_SOLUCAO; // PODE IR PARA ESQUERDA?
+        boolean podeDireita = y + 1 < labirinto[x].length && labirinto[x][y+1] != PAREDE && labirinto[x][y+1] != CAMINHO_SOLUCAO; // PODE IR PARA DIREITA?
+
+        // 4. Tenta as 4 direções
+        if(podeCima){ // Valida se pode ir para CIMA
             if(resolverLabirinto(x-1, y)) return true; // Vai para cima
         }
-        if(x + 1 < labirinto.length && labirinto[x+1][y] != PAREDE && labirinto[x][y] != CAMINHO_SOLUCAO){ // Valida se pode ir para baixo
+        if(podeBaixo){ // Valida se pode ir para BAIXO
             if(resolverLabirinto(x+1, y)) return true; // Vai para baixo
         }
-        if(y - 1 >= 0 && labirinto[x][y-1] != PAREDE && labirinto[x][y] != CAMINHO_SOLUCAO){ // Valida se pode ir para esquerda
+        if(podeEsquerda){ // Valida se pode ir para ESQUERDA
             if(resolverLabirinto(x, y-1)) return true; // Vai para esquerda
         } 
-        if(y + 1 < labirinto[x].length && labirinto[x][y+1] != PAREDE && labirinto[x][y] != CAMINHO_SOLUCAO){ // Valida se pode ir para direita
+        if(podeDireita){ // Valida se pode ir para DIREITA
             if(resolverLabirinto(x, y+1)) return true; // Vai para a direita
         }
         return false;
+    }
+
+    public void imprimeLabirinto(){
+        for(int linha = 0; linha < labirinto.length; linha++){
+            for (int coluna = 0; coluna < labirinto[0].length; coluna++) {
+                System.out.print(labirinto[linha][coluna]);
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/etapa1/PrincipalLabirinto.java
+++ src/main/java/etapa1/PrincipalLabirinto.java
@@ -3,6 +3,16 @@ package etapa1;
 public class PrincipalLabirinto {
     public static void main(String[] args) {
         Labirinto labirinto = new Labirinto();
-        labirinto.criaLabirinto("src/main/java/etapa1/labirinto.txt");
+
+        labirinto.criaLabirinto("src/main/java/etapa1/labirinto.txt"); // Carrega o labirinto a partir do arquivo de texto.
+        labirinto.imprimeLabirinto(); // Exibi o labirinto inicial.
+        
+        boolean encontrou = labirinto.percorreLabirinto();
+        if(encontrou){ // Executa a busca pela saída.
+            System.out.println("Solução encontrada");
+            labirinto.imprimeLabirinto(); // Exibi o labirinto final, mostrando o caminho da solução, se encontrado.
+        } else{
+            System.out.println("Caminho não encontrado!!");
+        }
     }
 }
